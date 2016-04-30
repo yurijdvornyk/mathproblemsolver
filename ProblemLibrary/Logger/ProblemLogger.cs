d47e3f19844c8e1ec53b62a3a926101078f6ad1b
yurijdvornyk <yurijdvornyk@gmail.com>
2016-04-30 20:34:55 +0300

    Add docs.

--- ProblemLibrary/Logger/ProblemLogger.cs
+++ ProblemLibrary/Logger/ProblemLogger.cs
@@ -6,10 +6,17 @@ using System.Threading.Tasks;
 
 namespace ProblemLibrary.Logger
 {
+    /// <summary>
+    /// Use this class to log information while executing problem
+    /// </summary>
     public class ProblemLogger
     {
         private static List<IProblemLogListener> listeners;
 
+        /// <summary>
+        /// Make you listener listen new logs appears
+        /// </summary>
+        /// <param name="listener">listener</param>
         public static void RegisterListener(IProblemLogListener listener)
         {
             if (!listeners.Contains(listener))
@@ -18,6 +25,10 @@ namespace ProblemLibrary.Logger
             }
         }
 
+        /// <summary>
+        /// Make you listener not to listen new logs appears
+        /// </summary>
+        /// <param name="listener">listener</param>
         public static void UnregisterListener(IProblemLogListener listener)
         {
             if (listeners.Contains(listener))
@@ -26,6 +37,10 @@ namespace ProblemLibrary.Logger
             }
         }
 
+        /// <summary>
+        /// Log info message
+        /// </summary>
+        /// <param name="message">message</param>
         public static void LogInfo(string message)
         {
             foreach (var listener in listeners)
@@ -34,6 +49,10 @@ namespace ProblemLibrary.Logger
             }
         }
 
+        /// <summary>
+        /// Log error message. Use this for handling exceptions, writing details for some errors and so on.
+        /// </summary>
+        /// <param name="message"></param>
         public static void LogError(string message)
         {
             foreach (var listener in listeners)
