d9cf6ac7cc1c74f24ee2ddf52c865f6c65919126
yurijdvornyk <yurijdvornyk@gmail.com>
2016-05-01 15:29:26 +0300

    Rename ProblemLibrary to ProblemDevelopmentKit.

--- ProblemLibrary/Properties/AssemblyInfo.cs
+++ ProblemLibrary/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ProblemLibrary")]
+[assembly: AssemblyTitle("Problem Development Kit")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ProblemLibrary")]
+[assembly: AssemblyProduct("Problem Development Kit")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
