0b2e4b722fce0eff10876db397da4aee6f4971d2
yurijdvornyk <yurijdvornyk@gmail.com>
2016-05-01 15:08:37 +0300

    Remove unused usings.

--- ProblemLibrary/Listener/IListener.cs
+++ ProblemLibrary/Listener/IListener.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ProblemLibrary.Listener
+﻿namespace ProblemLibrary.Listener
 {
     /// <summary>
     /// Base interface for listeners.
--- ProblemLibrary/Progress/ISolvingProgressListener.cs
+++ ProblemLibrary/Progress/ISolvingProgressListener.cs
@@ -1,9 +1,4 @@
 ﻿using ProblemLibrary.Listener;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProblemLibrary.Progress
 {
